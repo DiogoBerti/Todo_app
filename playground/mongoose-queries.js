d5a7bd6ac41170eff9fed97d478b3c2e279d7dc1
DiogoBerti <diogoberti88@gmail.com>
2018-08-07 21:10:14 -0300

    [ADD] -  Adicionadas rotas para o mongo e ajustes finos

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -22,19 +22,19 @@ const {User} = require('./../server/models/user');
 //   console.log('Todo', todo);
 // });
 
-// Todo.findById(id).then((todo) => {
-//   if (!todo) {
-//     return console.log('Id not found');
-//   }
-//   console.log('Todo By Id', todo);
-// }).catch((e) => console.log(e));
-
-User.findById('57bdb0fcdedf88540bfa2d66').then((user) => {
-  if (!user) {
-    return console.log('Unable to find user');
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return console.log('Id not found');
   }
+  console.log('Todo By Id', todo);
+}).catch((e) => console.log(e));
 
-  console.log(JSON.stringify(user, undefined, 2));
-}, (e) => {
-  console.log(e);
-});
+// User.findById('57bdb0fcdedf88540bfa2d66').then((user) => {
+//   if (!user) {
+//     return console.log('Unable to find user');
+//   }
+
+//   console.log(JSON.stringify(user, undefined, 2));
+// }, (e) => {
+//   console.log(e);
+// });
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose'); 
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -28,8 +29,24 @@ app.get('/todos', (req, res) => {
 	});
 });
 
-app.listen(3000, ()=>{
-	console.log('Started on port 3000');
+app.get('/todos/:id', (req, res) => {
+	
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+		    return res.send({'error': "Not Found"});
+		  }
+		  console.log('Todo By Id', todo);
+		  return res.send({todo});
+	}).catch((e) => res.send({'error': e}));
+});
+
+app.listen(port, ()=>{
+	console.log(`Started on port {port}`);
 });
 
 module.exports = {app};
